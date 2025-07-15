b522ea089b4287377c76949ea1402854fcc92e69
renatam14 <irenata0711@gmail.com>
2025-07-15 11:34:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@10.4.0/dist/mermaid.esm.min.mjs";
+
+mermaid.initialize({
+  startOnLoad: true,
+  theme: "default",
+  themeVariables: {
+    primaryColor: "#f48fb1",
+    primaryTextColor: "#4a148c",
+    fontFamily: "Times New Roman",
+    edgeLabelBackground: "#fff"
+  }
+});
